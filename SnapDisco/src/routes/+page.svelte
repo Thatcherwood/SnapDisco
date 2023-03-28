420fd230e0c9cff2b2ca260e463191d0a596b17c
Zoki138 <thatcherwood@protonmail.com>
2023-03-27 20:53:04 -0500

    Create misc web pages, and start work on home page

--- SnapDisco/src/routes/+page.svelte
+++ SnapDisco/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to DiscoSnap!</h1>
+<p>Visit <a href="/about">About</a> to read about the web app!</p>
+<p>Or <a href="/login">login</a></p>
+
