0638dfd7a7e7c2c244815d1eface17e16798de7e
Zoki138 <thatcherwood@protonmail.com>
2023-04-03 20:11:22 -0500

    Begin work on setting up cookies for user sessions. more login stuff. yay!

---
+++ SnapDisco/src/routes/signup/+page.svelte
@@ -0,0 +1,11 @@
+<form method="POST">
+    <label>
+      Email
+      <input name="email" type="email">
+    </label>
+    <label>
+      Password
+      <input name="password" type="password">
+    </label>
+    <button>Sign up</button>
+  </form>
