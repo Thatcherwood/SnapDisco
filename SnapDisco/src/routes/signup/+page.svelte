20a46f9e7d91d6de556399dd59f00b8d86b10d9e
Zoki138 <thatcherwood@protonmail.com>
2023-04-15 14:56:25 -0500

    password match validation now works

--- SnapDisco/src/routes/signup/+page.svelte
+++ SnapDisco/src/routes/signup/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { showPasswordWarn } from './+page.ts'
+	import { passwordMatchWarning, } from './signup';
 
 	export let newUsername = '';
 
@@ -10,42 +9,35 @@
 
 	let verifyPassword = '';
 
-	onMount(() => {
-		let verifyPassword1 = document.getElementsByName('password');
-		let verifyPassword2 = document.getElementsByName('password-verify');
-		let passwordMatch = false;
-
-		showPasswordWarn();
-
-		if (verifyPassword1 === verifyPassword2) {
-			passwordMatch = true; //TODO read this value on submit!
-			console.log('they match');
-		}
-	});
 </script>
 
 <h1>New Account</h1>
 
 <form method="POST">
-	<style>
-
-		#match-password {
-			visibility: hidden;
-		}
-
-	</style>
 	<label>
 		<p>Enter Username:</p>
-		<input name="username" bind:value={newUsername} placeholder="username" />
-
+		<input
+			name="username"
+			bind:value={newUsername}
+			placeholder="username"
+		/>
 		<p>Enter Email:</p>
-		<input name="email" bind:value={newUserEmail} placeholder="email" type="email" required />
-
+		<input
+			name="email"
+			bind:value={newUserEmail}
+			placeholder="email"
+			type="email"
+			required
+		/>
 		<p>Enter Password:</p>
-		<input name="password" bind:value={password} placeholder="password" type="password" required />
-
-		<div id="password-location" />
-
+		<input
+			name="password"
+			bind:value={password}
+			placeholder="password"
+			type="password"
+			required
+			on:change={() => passwordMatchWarning(password, verifyPassword)}
+		/>
 		<p>Re-Enter Password:</p>
 		<input
 			name="password-verify"
@@ -53,11 +45,12 @@
 			placeholder="password"
 			type="password"
 			required
+			on:change={() => passwordMatchWarning(password, verifyPassword)}
 		/>
-		<p id="match-password">The Passwords must match!</p>
+		<p id="match-password" style="visibility: visible">The Passwords must match!</p>
 
 		<br />
 		<br />
-		<input type="submit" value="submit" />
+		<input id="submission-button" type="submit" value="submit" />
 	</label>
 </form>
--- SnapDisco/src/routes/signup/+page.ts
+++
@@ -1,5 +0,0 @@
-
-function showPasswordWarn() {
-  const currentDiv = document.getElementById("password-location");
-  currentDiv?.setAttribute("style", "visibility: visible")
-}
