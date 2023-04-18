d2098e87ee29f30c37a14c509bddb0c8b358e207
Zoki138 <thatcherwood@protonmail.com>
2023-04-18 11:38:36 -0500

    Lots of work on website frontend

--- SnapDisco/src/routes/+page.svelte
+++ SnapDisco/src/routes/+page.svelte
@@ -1,6 +1,44 @@
-<h1>Welcome to DiscoSnap!</h1>
-<p>Visit <a href="/about">About</a> to read about the web app!</p>
-<p><a href="/login">login</a></p>
+<script lang="ts">
 
-<p><a href="/upload">upload</a></p>
-<p><a href="/signup">signup</a></p>
+</script>
+
+<body>
+	<h1>Welcome&nbsp;to&nbsp;SnapDisco!</h1>
+
+	<p>
+		SnapDisco is a website used to make small edits to video files for the purpose of reducing their
+		upload size. It is a mobile-friendly way to upload videos
+	</p>
+
+	<br />
+	<div style="text-align:center">
+		<a class="bigUploadBtn" href="/upload">Upload Your Video</a>
+	</div>
+</body>
+
+<style>
+	body {
+		background-color: rgba(247, 247, 14, 0.212);
+	}
+
+	h1 {
+		color: rgb(0, 21, 80);
+		text-align: center;
+	}
+
+	p {
+		color: rgb(0, 21, 80);
+		text-align: center;
+	}
+
+	.bigUploadBtn {
+		background-color: aliceblue; 
+		color: rgb(0, 21, 80);
+		border-style: dashed;
+		font-size: 4vw;
+		margin: 10px;
+		padding: 10px;
+		display: inline-block;
+        text-decoration: none;
+	}
+</style>
---
+++ SnapDisco/src/routes/upload/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  /*const handleFileUpload = async (event: SubmitEvent) => {
+    console.log('submit')
+  } */ // see https://www.okupter.com/blog/svelte-file-upload
+</script>
+
+
+<form on:submit> <!-- |preventDefault={handleFileUpload}> -->
+	<label for="file">Upload your video here.</label>
+	<input type="file" id="file" name="file" accept=".mp4" required />
+	<button class="btn" type="submit">Submit</button>
+</form>
+<p>Only ".mp4" files are accepted.</p>
+
+<style>
+
+    .btn {
+
+        background-color: aliceblue;
+
+    }
+</style>
